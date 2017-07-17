a2a2e485b5ae2c45475bdd957fea7dedf999d127
kaalm1 <kaalm1@gmail.com>
2017-07-17 19:15:25 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musician,instruments){
+  var arr = []
+  for(var i=0;  i < musician.length;i++){
+    arr.push(`${musician[i]} plays ${instruments[i]}`)
+  }
+  return arr
+
+}
+
+function johnLennonFacts(facts){
+  var arr = []
+  var i = 0
+  while (i<facts.length) {
+    arr.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return arr
+}
+
+function iLoveTheBeatles(num){
+  var arr = []
+  do {
+    arr.push('I love the Beatles!')
+    num++
+  } while (num<15);
+  return arr
+}
